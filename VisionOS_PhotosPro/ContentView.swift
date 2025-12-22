a4a18ca7872f0fb0a7996b3212ae9018d9da5a9a
Roger Navarro <roanacla@me.com>
2025-12-22 11:18:51 -0800

    Initial Commit

---
+++ VisionOS_PhotosPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisionOS_PhotosPro
+//
+//  Created by Roger Navarro Claros on 12/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionOS_PhotosPro/VisionOS_PhotosProApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOS_PhotosProApp.swift
+//  VisionOS_PhotosPro
+//
+//  Created by Roger Navarro Claros on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionOS_PhotosProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
